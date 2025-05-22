f7537402336521cddf2324618277790bdcc560e6
DVectors <drvectorartworks@gmail.com>
2025-05-22 19:41:26 +0100

    [ADD] Added wall detection for enemy. Enemy will now move the other way when coming into contact with wall.

--- Resources/Scripts/Enemies/enemy.gd
+++ Resources/Scripts/Enemies/enemy.gd
@@ -9,13 +9,15 @@ class_name Enemy
 @export var can_fly: bool = false
 @export var can_pass_walls: bool = false
 @export var is_platform: bool = false
-@export var can_turn_around: bool = false
+@export var can_turn_from_edge: bool = false
 @export_enum("Left", "Right") var start_direction: String = "Left"
 
 var direction: int
+var animated_sprite_2d: AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	animated_sprite_2d = get_node("AnimatedSprite2D")
 	direction = set_start_direction()
 	
 func set_start_direction() -> int:
@@ -30,6 +32,8 @@ func _process(delta: float) -> void:
 	
 func _physics_process(delta: float) -> void:
 	handle_gravity(delta)
+
+	detect_wall()
 	
 	velocity.x = direction * speed
 	
@@ -41,4 +45,16 @@ func handle_gravity(delta: float) -> void:
 			velocity += get_gravity() * delta # Apply gravity
 			
 func detect_wall() -> void:
+	if is_on_wall():
+		if direction == -1: #Left
+			_flip_direction()
+			direction = 1
+		elif direction == 1: #Right
+			_flip_direction()
+			direction = -1
+
+func _flip_direction():
+	animated_sprite_2d.flip_h = not animated_sprite_2d.flip_h
+	
+func on_death() -> void:
 	pass
