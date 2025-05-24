e5a4c26094e0d47841003223bfc994a5be412e78
DVectors <drvectorartworks@gmail.com>
2025-05-24 08:06:38 +0100

    [ADD] Added error handler for load_resources_csv_to_dict in CSVReader.gd and created method for playing random sound in SoundManager.gd

--- Resources/Scripts/Audio/SoundManager.gd
+++ Resources/Scripts/Audio/SoundManager.gd
@@ -19,12 +19,22 @@ func __setup_audio_players() -> void:
 		audio_players[key] = audio_player
 	
 static func play(sound: String) -> void:
-	assert(sound in SOUND_EFFECTS)
+	__play_sound(sound)
 	
+static func play_random(sound_list: Array[String]) -> void:
+	if !sound_list.is_empty() and sound_list != null:
+		sound_list.shuffle() # Shuffle to randomise list order - Should increase chance of non regular sound pick
+		var sound: String = sound_list[0]
+		
+		__play_sound(sound)
+		
+static func __play_sound(sound: String) -> void:
+	assert(sound in SOUND_EFFECTS)
+
 	if audio_players.has(sound):
 		var player = audio_players[sound]
 		player.stream = SOUND_EFFECTS[sound]
-		
+
 		player.play()
 	else:
 		printerr("Invalid Audio Player - Does not exist!")
--- Resources/Scripts/Utilities/CSVReader.gd
+++ Resources/Scripts/Utilities/CSVReader.gd
@@ -5,6 +5,8 @@ class_name CSVReader
 static func load_resources_csv_to_dict(path: String) -> Dictionary:
 	var parsed_dict: Dictionary = {}
 	var file: FileAccess = FileAccess.open(path, FileAccess.READ)
+	
+	assert(file != null, "ERROR: failed to find file at path: %s" % path)
 
 	if file != null:
 		file.get_csv_line() # Skip headers
