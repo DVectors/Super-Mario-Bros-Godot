7cbeae1d278e6021b8143b27873c73648ca1868c
DVectors <drvectorartworks@gmail.com>
2025-06-06 20:51:40 +0100

    [ADD] Updated HUD UI by adding a coin and Mario head icon to represent coins and lives respectively. Also added ability to see the current amount of lives on the HUD.

--- Resources/Scripts/HUD/HUDManager.gd
+++ Resources/Scripts/HUD/HUDManager.gd
@@ -4,7 +4,8 @@ class_name HUDManager
 
 @onready var player_label: RichTextLabel = $PlayerLabel
 @onready var player_score_label: RichTextLabel = $PlayerLabel/PlayerScoreLabel
-@onready var coins_amount_label: RichTextLabel = $CoinsLabel/CoinAmountLabel
+@onready var coins_amount_label: RichTextLabel = $CoinAmountLabel
+@onready var lives_amount_label: RichTextLabel = $LivesAmountLabel
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,4 +14,5 @@ func _process(delta: float) -> void:
 	# Mario score tally has a digit count of 7, so this will pad the score value with 7 digits
 	player_score_label.text = str(GameInstanceManager.get_points()).pad_zeros(7) 
 	coins_amount_label.text = "x%s" % str(GameInstanceManager.get_coins()).pad_zeros(2)
+	lives_amount_label.text = "x%s" % str(GameInstanceManager.get_lives()).pad_zeros(2)
 	
